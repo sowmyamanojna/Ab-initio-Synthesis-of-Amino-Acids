db82b7de05d9ca5b039fd97707d77706a4e968f2
sowmyamanojna <sowmyamanojna@gmail.com>
2020-07-02 17:29:53 +0530

    Removed old RMG parser files

--- code/old_files/parse_data_rmg_v2/calculate_properties.py
+++
@@ -1,9 +0,0 @@
-import os
-import csv
-import numpy as np
-import pandas as pd
-import scipy.constants as sc
-
-data = pd.read_csv('data.csv')
-data = data.T.reset_index().T
-data = data.rename(columns={1:})
